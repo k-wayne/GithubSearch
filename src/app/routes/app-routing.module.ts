35ffcacd3e1305ae61b32984e52d4110e74b6163
kwayne <wkudoi@gmail.com>
2019-10-02 00:13:49 +0300

    added route to about page pop up

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GithubComponent } from '../components/github/github.component';
+import { SearchComponent } from '../components/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'goals', component: GithubComponent },
+  { path: 'about', component: SearchComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
