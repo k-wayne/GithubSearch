1e7d0513867a61db0d4d98370cde0395f64ff01e
kwayne <wkudoi@gmail.com>
2019-10-01 16:43:37 +0300

    created a console text to affirm services working

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -4,25 +4,30 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class GithubService {
 
   private username: string;
   private clientid = '55906159767228560208';
   private clientsecret = '80b91175b387183acd2862a0caeb5f8d17d5625d';
 
   constructor(private http: HttpClient) {
-    console.log('Hurray. You did it!');
+    console.log('Serving...!');
     this.username = 'k-wayne';
   }
 
   getProfileInfo() {
-    // tslint:disable-next-line: max-line-length
+    interface ApiResponse {
+      login: string;
+    }
+    // tslint:disable-next-line:max-line-length
     return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.clientid + '&client_secret=' + this.clientsecret);
   }
+
   getProfileRepos() {
-    // tslint:disable-next-line: max-line-length
+    // tslint:disable-next-line:max-line-length
     return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id=' + this.clientid + '&client_secret=' + this.clientsecret);
   }
+
   updateProfile(username: string) {
     this.username = username;
   }
